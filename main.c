04781264dd0a9682f55ccf35e6dfa3e8c1c84262
evyatar4499 <evyatar4499@gmail.com>
2023-03-23 16:46:22 +0200

    progress update

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "linkedlist.h"
-#include ""
+#include "linkedlist.c"
+#include "IO.h"
+#include "IO.c"
+
+int main(){
+    int exit = 0;
+    while (exit == false){
+        /*blabla*/
+
+    }
+    return 0;
+}
